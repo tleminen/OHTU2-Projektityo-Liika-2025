63fb02bac1005a99c587c42f69bc16d2d6953544
Tomi Leminen <tleminen@student.uef.fi>
2025-02-04 16:30:20 +0200

    kartta näkyvissä etusivulla

--- frontend/src/components/frontpageVIew/frontpage.jsx
+++ frontend/src/components/frontpageVIew/frontpage.jsx
@@ -1,11 +1,17 @@
 import LoginForm from "../LoginForm"
-
+import Map from "../mapView/map"
 const Frontpage = () => {
   return (
+    
     <div>
       <h1>Frontpage placeholder</h1>
       <LoginForm/>
+      <div>
+        <Map />
+      </div>
     </div>
+
+    
 
 
 )
