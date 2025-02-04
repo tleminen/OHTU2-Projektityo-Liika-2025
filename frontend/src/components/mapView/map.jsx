765aabf2b69535106de3c0fd233ef9010bdb2507
Tomi Leminen <tleminen@student.uef.fi>
2025-02-04 16:16:38 +0200

    karttaValmis

--- frontend/src/components/mapView/map.jsx
+++ frontend/src/components/mapView/map.jsx
@@ -1,1 +1,31 @@
-console.log("Pusseissani tunnen sen tii ti ti tiii")
+import { useEffect } from "react"
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
+import "../../index.css"
+const Map = () => {
+  useEffect(() => {
+    const mapContainer = document.createElement("div")
+    mapContainer.id = "map"
+    document.body.appendChild(mapContainer)
+
+    console.log(`map useEffect`)
+    // Luo karttaelementti kun komponentti mounttaa
+    const map = L.map("map", {
+      center: [62.6013, 29.7639], // Joensuun koordinaatit
+      zoom: 10,
+    })
+
+    // Lisää karttalaatta OpenStreetMapista
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map)
+
+    return () => {
+      // Tuhoaa karttaelementin kun komponentti unmounttaa
+      map.remove()
+    }
+  }, [])
+}
+
+export default Map
