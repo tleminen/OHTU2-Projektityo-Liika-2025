0413b35e33f05c73f58f46e2d178bd50f40c9d9b
Annakaisa Turunen <annaturu@student.uef.fi>
2025-02-04 15:07:58 +0200

    kirjautumisnäkymää part 2

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,11 +1,41 @@
+import { use } from 'react'
 import { useState } from 'react'
 
 const LoginForm = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('Login attempt:', { username, password })
+    }
+
     return (
         <div>
-            <p>
-                tähän tulee login form 
-            </p>
+
+            <form OnSubmit={handleSubmit}>
+                <div>
+                    <label>Käyttäjänimi:</label>
+                    <input
+                    type="text"
+                    value={username}
+                    name='username'
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder='kirjoita käyttäjänimi tähän'
+                    />
+                </div>
+                <div>
+                    <label>Salasana:</label>
+                    <input
+                    type= "password"
+                    value={password}
+                    name='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder='kirjoita salasana tähän'
+                    />
+                    </div>
+                <button type ="submit">Kirjaudu sisään</button>
+            </form>
         </div>
     )
 }
