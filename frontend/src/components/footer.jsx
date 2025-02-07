3093a2d2f65cecf2bd0a7f37dd430b5d4f542fa7
Olli Hilke <hilkolli@student.uef.fi>
2025-02-07 09:47:53 +0200

    footer kielivalinta

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,8 +1,8 @@
 import "../index.css"
 import { useSelector } from "react-redux"
 import translations from "../assets/translation.js"
-import Select from 'react-select'
-import { useState } from 'react';
+import Select from "react-select"
+import { useState } from "react"
 
 const Footer = () => {
   const language = useSelector((state) => state.language.language)
@@ -29,29 +29,30 @@ const Footer = () => {
 
 // Esimerkki lippuvalintakomponentista (korvaa omalla toteutuksellasi)
 const Lippuvalinta = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(null)
 
   const options = [
-    { value: 'fi', label: 'Suomi' },
-    { value: 'en', label: 'Englanti' },
-    // TODO: Vaihda hakemaan kielet 
-  ];
+    { value: "fi", label: "Suomi" },
+    { value: "en", label: "Englanti" },
+    // TODO: Vaihda hakemaan kielet
+  ]
 
   const handleChange = (selectedOption) => {
-    setSelectedOption(selectedOption);
-    console.log(`Valittu kieli: ${selectedOption.value}`);
+    setSelectedOption(selectedOption)
+    console.log(`Valittu kieli: ${selectedOption.value}`)
     // Tässä voitaisiin tehdä toimintoja, jotka liittyvät kielen vaihtamiseen
-  };
+  }
 
   return (
     <div>
-      <Select menuPlacement="top"
+      <Select
+        menuPlacement="top"
         value={selectedOption}
         onChange={handleChange}
         options={options}
       />
     </div>
-  );
+  )
 }
 
 // Esimerkki infokomponentista (korvaa omalla toteutuksellasi)
