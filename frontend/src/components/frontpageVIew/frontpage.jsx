967b3222b916162596624bf63d2e90a857d4fd36
Olli Hilke <hilkolli@student.uef.fi>
2025-02-11 12:08:21 +0200

    signedOut renderöinnin ehdollisuutta

--- frontend/src/components/frontpageVIew/frontpage.jsx
+++ frontend/src/components/frontpageVIew/frontpage.jsx
@@ -2,15 +2,12 @@ import Header from "../header"
 import mapImage from "../../assets/map_kuvituskuva.png"
 import Footer from "../footer"
 import { useNavigate } from "react-router-dom"
-import { useSelector } from "react-redux"
-import translations from "../../assets/translation"
+import SignedOut from "./signedOut"
 const Frontpage = () => {
   const navigate = useNavigate()
   const navigateTo = (path) => {
     navigate(`/${path}`)
   }
-  const language = useSelector((state) => state.language.language)
-  const t = translations[language]
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
@@ -22,10 +19,7 @@ const Frontpage = () => {
         height={100}
         onClick={() => navigateTo("map")}
       />
-      <div className="containerX">
-        <button onClick={() => navigateTo("login")}>{t.login}</button>
-        <button onClick={() => navigateTo("register")}>{t.register}</button>
-      </div>
+      <SignedOut />
 
       <div>
         <Footer />
