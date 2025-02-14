258c4f364ca6772ba21a083e31efc5be96b36838
Tomi Leminen <tleminen@student.uef.fi>
2025-02-14 02:47:39 +0200

    oAuth2.0

--- backend/tests/testEmail.js
+++ backend/tests/testEmail.js
@@ -2,13 +2,27 @@ require('dotenv').config(); // Lataa ympäristömuuttujat
 
 const nodemailer = require('nodemailer');
 
+//TODO: Pitää alkaa käyttämään modernia todennusta (OAuth 2.0)
+
 const transporter = nodemailer.createTransport({
+  /*
   service: 'outlook', // Vaihda oikeaan sähköpostipalveluun
   auth: {
     user: 'liika2025@outlook.com',
     pass: 'hkrcdcnunlnstnma', // Tai perussalasana, jos käytät sitä
 
   },
+  */
+
+  service: 'outlook',
+    auth: {
+        type: 'OAuth2',
+        user: 'liika2025@outlook.com', // Sähköpostisi
+        clientId: 'YOUR_CLIENT_ID', // Azure AD:sta
+        clientSecret: 'YOUR_CLIENT_SECRET', // Azure AD:sta
+        refreshToken: 'YOUR_REFRESH_TOKEN', // Hankittu aiemmin
+        accessToken: 'YOUR_ACCESS_TOKEN', // Voit hankkia tämän refresh tokenilla
+    },
   
 });
 
