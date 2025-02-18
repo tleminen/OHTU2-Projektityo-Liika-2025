f1c5e86ce1003215d8168bebe5ab18546ca297c4
Tomi Leminen <tleminen@student.uef.fi>
2025-02-18 12:37:33 +0200

    awderfew

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -5,7 +5,7 @@ let POSTGRESQL_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_POSTGRESQL_URI
     : process.env.POSTGRESQL_URI
-
+    
 module.exports = {
   POSTGRESQL_URI,
   PORT,
