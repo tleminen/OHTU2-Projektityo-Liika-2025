5eb15bf66f4cfcb33d5dbf84687043c911e03f29
Olli Hilke <hilkolli@student.uef.fi>
2025-02-20 19:54:56 +0200

    icons toimii, tällähetkellä, ei katogorioittain

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -5,133 +5,133 @@ const ICONANCHOR = [32, 62]
 const POPUPANCHOR = [-3, -76]
 
 export var amerikkalainen_jalkapallo = L.icon({
-  iconUrl: "/assets/lajit/amerikkalainen_jalkapallo.png", // Tulee olla public-kansiossa
+  iconUrl: "/lajit/amerikkalainen_jalkapallo.png", // Tulee olla public-kansiossa
   iconSize: ICONSIZE, // Ikonin koko pixeleinä
   iconAnchor: ICONANCHOR, // Minkä verran offsettiä painalluskohdasta
   popupAnchor: POPUPANCHOR, // -""-
 })
 
 export var avantouinti = L.icon({
-  iconUrl: "/assets/lajit/avantouinti.png",
+  iconUrl: "/lajit/avantouinti.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var biljardi = L.icon({
-  iconUrl: "/assets/lajit/biljardi.png",
+  iconUrl: "/lajit/biljardi.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var golf = L.icon({
-  iconUrl: "/assets/lajit/golf.png",
+  iconUrl: "/lajit/golf.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var jaakiekko = L.icon({
-  iconUrl: "/assets/lajit/jaakiekko.png",
+  iconUrl: "/lajit/jaakiekko.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var jalkapallo = L.icon({
-  iconUrl: "/assets/lajit/jalkapallo.png",
+  iconUrl: "/lajit/jalkapallo.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var juoksu = L.icon({
-  iconUrl: "/assets/lajit/juoksu.png",
+  iconUrl: "/lajit/juoksu.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var keilaus = L.icon({
-  iconUrl: "/assets/lajit/keilaus.png",
+  iconUrl: "/lajit/keilaus.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var koripallo = L.icon({
-  iconUrl: "/assets/lajit/koripallo.png",
+  iconUrl: "/lajit/koripallo.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var lentopallo = L.icon({
-  iconUrl: "/assets/lajit/lentopallo.png",
+  iconUrl: "/lajit/lentopallo.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var nyrkkeily = L.icon({
-  iconUrl: "/assets/lajit/nyrkkeily.png",
+  iconUrl: "/lajit/nyrkkeily.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var padel = L.icon({
-  iconUrl: "/assets/lajit/padel.png",
+  iconUrl: "/lajit/padel.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var pesapallo = L.icon({
-  iconUrl: "/assets/lajit/pesapallo.png",
+  iconUrl: "/lajit/pesapallo.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var pingis = L.icon({
-  iconUrl: "/assets/lajit/pingis.png",
+  iconUrl: "/lajit/pingis.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var puntti = L.icon({
-  iconUrl: "/assets/lajit/puntti.png",
+  iconUrl: "/lajit/puntti.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var pyoraily = L.icon({
-  iconUrl: "/assets/lajit/pyoraily.png",
+  iconUrl: "/lajit/pyoraily.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var sulkapallo = L.icon({
-  iconUrl: "/assets/lajit/sulkapallo.png",
+  iconUrl: "/lajit/sulkapallo.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var tennis = L.icon({
-  iconUrl: "/assets/lajit/tennis.png",
+  iconUrl: "/lajit/tennis.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
 })
 
 export var uinti = L.icon({
-  iconUrl: "/assets/lajit/uinti.png",
+  iconUrl: "/lajit/uinti.png",
   iconSize: ICONSIZE,
   iconAnchor: ICONANCHOR,
   popupAnchor: POPUPANCHOR,
