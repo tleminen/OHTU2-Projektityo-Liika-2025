4c990e321667a9732424d20455a3205ef0ad5d24
Annakaisa Turunen <annaturu@student.uef.fi>
2025-02-25 15:23:20 +0200

    uusi sähköposti

--- frontend/src/components/accountView/changeInfo/changeEmail.jsx
+++ frontend/src/components/accountView/changeInfo/changeEmail.jsx
@@ -4,59 +4,41 @@ import Header from "../../header"
 import translations from "../../../assets/translation.js"
 import { useSelector } from "react-redux"
 import "../../../index.css"
+import "../accountView.css"
 
 const ChangeEmail = () => {
 
   const language = useSelector((state) => state.language.language)
   const t = translations[language]
   const [newEmail, setNewEmail] = useState("")
-
-  const options = [
-    { value: "FI", label: "Suomi" },
-    { value: "EN", label: "English" },
-    //Lisätään muita kieliä tarvittaessa
-  ]
-
-  const selectedOption = options.find(
-    (option) => option.value === selectedLanguage
-  )
+  const email = useSelector((state) => state.user.user.email)
 
   const handleSubmit = (event) => {
     event.preventDefault()
     console.log("Create event attempt:", {
       newEmail
     })
-  
-
-
-  const language = useSelector((state) => state.language.language)
-  const t = translations[language]
-  const [newEmail, setNewEmail] = useState("")
-
-  const options = [
-    { value: "FI", label: "Suomi" },
-    { value: "EN", label: "English" },
-    //Lisätään muita kieliä tarvittaessa
-  ]
-
-  const selectedOption = options.find(
-    (option) => option.value === selectedLanguage
-  )
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log("Create event attempt:", {
-      newEmail
-    })
+  }
   
 
   return (
     
-    <div className="fullpage">
+    <div className="fullpage"
+    style={{
+      backgroundImage: "url('/backgroundpicture.jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}>
+      
       <Header />
+      <div className="account-view">
       <h1>
         Vaihda sähköposti
       </h1>
+      <p>
+        Nykyinen sähköpostiosoite: {email}
+      </p>
       <form onSubmit={handleSubmit}>
         <div>
           <h3>{t.newEmail}</h3>
@@ -84,12 +66,13 @@ const ChangeEmail = () => {
         </div>
         <button type="submit">{t.save}</button>
         </form>
+        </div>
         <Footer />
         </div>
         )
         }
-      }
-  
+      
+    
 export default ChangeEmail
 
 
