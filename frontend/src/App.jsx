2c2ea35b4872f7df5b4904b6c3b5c05863ac9e88
Tomi Leminen <tleminen@student.uef.fi>
2025-03-05 15:06:40 +0200

    muutos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,29 +19,25 @@ import { changeLanguage } from "./store/languageSlice"
 
 const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Frontpage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/map" element={<MapView />} />
+      <Route path="/termsOfService" element={<TermsOfServiceView />} />
+      <Route path="/info" element={<InfoView />} />
+      <Route path="/create_event" element={<CreateEventView />} />
+      <Route path="/own_info" element={<AccountView />} />
+      <Route path="/events/:id" element={<EventView />} />
+      <Route path="/own_info/email" element={<ChangeEmail />} />
+      <Route path="/own_info/username" element={<ChangeUsername />} />
+      <Route path="/joined_events" element={<JoinedView />} />
+      <Route path="/own_info/password" element={<ChangePassword />} />
+      <Route path="/created_events" element={<CreatedEvents />} />
+      <Route path="/events/own/:id" element={<ModifyEvent />} />
+      <Route path="/own_info/language" element={<ChangeLanguage />} />
+    </Routes>
   )
 }
 
-export default App;
+export default App
