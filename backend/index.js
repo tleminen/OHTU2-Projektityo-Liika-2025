0ac926815f94246a19f5c4a8c8f9912eb20b291b
Olli Hilke <hilkolli@student.uef.fi>
2025-03-06 15:19:52 +0200

    rollback

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const app = require("./app")
 const config = require("./utils/config")
+const path = require("path")
+app.use(express.static(path.join(__dirname, "public")))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
 
 app.listen(config.PORT, () => {
   console.log(`Serveri on käynnissä portissa ${config.PORT}`)
