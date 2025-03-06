b403df53e4db19695efb89b0ad635c438be339fd
Olli Hilke <hilkolli@student.uef.fi>
2025-03-07 01:05:39 +0200

    ja testiä taas

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: { "/api": "http://localhost:3003" },
-  },
+  server: {},
   build: {
     outDir: "../backend/public",
   },
